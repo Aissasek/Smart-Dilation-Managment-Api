6b8a2134ff3480b874d55d967a084e4f2bfff621
Aissasek <proocafo@example.com>
2026-02-02 15:11:55 +0100

    add .gitignore

--- obj/Debug/net9.0/Smart Dilation Management.AssemblyInfo.cs
+++ obj/Debug/net9.0/Smart Dilation Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smart Dilation Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b5b39e3da20ae18ff3419c3638ef676f81acc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1edf6c3a98af747c2fef44f83fcea0ec96470c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smart Dilation Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smart Dilation Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
